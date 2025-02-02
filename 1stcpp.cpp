3a76579efded49ded51920dcc947531bea78af5f
abhimanyukumar <abhimanyucode2021@gmail.com>
2025-02-02 18:38:09 +0530

    1st class complete

---
+++ 1stcpp.cpp
@@ -0,0 +1,18 @@
+//  AARRAAYYYY
+ 
+ 
+        // #include<iostream>
+        // using namespace std;
+
+        // int main(){
+
+        // //  int arr[5]= {3, 4, 3, 5, 3};
+
+
+        // // for(int i = 0 ; i < 5 ; i++){
+        // // cout << arr[i]<<" ";
+
+        // // }
+        // // cout << endl;
+            
+        // }
