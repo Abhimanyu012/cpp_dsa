b87594fc45b7f1656be8052081fde62c463dfbcb
Abhimanyu012 <abhimanyucode2021@gmail.com>
2025-02-06 01:11:45 +0530

    sqaure start pattern

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void print1()
+{
+    int n = 5;
+    // cin >> n;
+    cout<<"this is oue square pattern: "<<endl;
 
-    
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+          
+        }
+        cout << endl;
+        
+    }
+}
+int main()
+{
+    print1();
+
     return 0;
 }
