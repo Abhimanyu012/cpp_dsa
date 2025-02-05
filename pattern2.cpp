a73f3846b420223983018570523b16c51d721ecb
Abhimanyu012 <abhimanyucode2021@gmail.com>
2025-02-06 01:48:26 +0530

    4 types of patter have been commited

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,26 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print1()
+void pattern1()
 {
     int n = 5;
     // cin >> n;
-    cout<<"this is oue square pattern: "<<endl;
+    cout << "this is oue square pattern: " << endl;
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
             cout << "* ";
-          
         }
         cout << endl;
-        
+    }
+}
+void pattern2()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void pattern3()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+
+            cout << i << " ";
+        }
+        cout << i;
+        cout << endl;
     }
 }
 int main()
 {
-    print1();
+    pattern3();
 
     return 0;
 }
