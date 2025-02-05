d1e4bca0fbc70f4a481131d5462ae996e83ac502
Abhimanyu012 <abhimanyucode2021@gmail.com>
2025-02-06 02:12:26 +0530

    agian next  pattern added

--- pattern2.cpp
+++ pattern2.cpp
@@ -45,9 +45,24 @@ void pattern3()
         cout << endl;
     }
 }
+
+void pattern5(){
+     int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <=n-i+1; j++) // j<i
+        {
+
+            cout <<"* ";
+        }
+       cout << endl;
+    }
+}
 int main()
 {
-    pattern3();
+    pattern5();
 
     return 0;
 }
